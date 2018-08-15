8297dc81ee70b22b5c29d534100ed8b62a5c889d
Sagor Chowdhuri <sagorchowdhuri@gmail.com>
2018-08-15 21:06:21 +0530

    Socket Io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const path=require('path');
 const express=require('express');
 
+//SOCKET IO
+const http=require('http');
+const socketIO=require('socket.io');
+const server=http.createServer();
+const io=socketIO(server);
+io.on('connection',function(client){
+    console.log('New user connected');
+});
+
 const port=process.env.PORT || 3000;
 
 var publicDir=path.join(__dirname, '../public');
 var app=express();
 app.use(express.static(publicDir));
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`server is up on ${port}`);
 })
