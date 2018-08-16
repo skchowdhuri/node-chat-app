2db42e616c92bd074d49a1e415f44920af1495b9
Sagor Chowdhuri <sagorchowdhuri@gmail.com>
2018-08-17 00:09:42 +0530

    a

--- server/server.js
+++ server/server.js
@@ -4,18 +4,33 @@ const express=require('express');
 //SOCKET IO
 const http=require('http');
 const socketIO=require('socket.io');
-const server=http.createServer();
-const io=socketIO(server);
-io.on('connection',function(client){
-    console.log('New user connected');
-});
 
 const port=process.env.PORT || 3000;
 
 var publicDir=path.join(__dirname, '../public');
 var app=express();
+const server=http.createServer(app);
+const io=socketIO(server);
 app.use(express.static(publicDir));
 
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect', function(){
+        console.log('User Disconnected');
+    });
+    socket.emit('newEmail',{
+        name: 'Sagor',
+        email: 'hh@gmail.com'
+    });
+    socket.on('createMeassage',(message)=>{
+        console.log('message',message);
+        io.emit('newMessage',{
+            name: 'Sagor',
+            message:'Hy! whats upp!!',
+            createdAt: new Date().getTime()
+        })
+    });
+});
 server.listen(port,()=>{
     console.log(`server is up on ${port}`);
-})
+});
